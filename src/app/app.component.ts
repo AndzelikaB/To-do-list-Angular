27f837c7d2cfa0a435866b8820a7efda82229891
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-09-25 22:08:50 +0200

    learn 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {ReversePipe} from 'ngx-pipes';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers: [ReversePipe]
 })
+
 export class AppComponent {
-  title = 'Lista zadań';
+
+  // constructor(private reversePipe: ReversePipe) {
+  //   this.reversePipe.transform('foo'); 
+  // }
+
+  someDate = Date.now();
+
+  data(){
+    Date.now();
+  }
+  title = "Przykłady-pipów"
+  test = 'Data'
+  dzien = 'Dzień'
+  number = 7.0890990909765;
+  price = 2.99;
+  percentage = 100;
+
+  translate = {
+    zima: 'winter',
+    wiosna: 'spring',
+    jesień: 'autumn',
+    lato: 'summer',
+  };
+
+  words = 'Codylenek'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { registerLocaleData } from '@angular/common';
+import PL from '@angular/common/locales/pl';
+import de from '@angular/common/locales/de'
+
+registerLocaleData(PL);
+registerLocaleData(de);
+
+import {NgPipesModule} from 'ngx-pipes';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgPipesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
+
+
